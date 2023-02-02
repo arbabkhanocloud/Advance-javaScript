a828888bc52ddc37af56b3560601951602b3f016
Arbab Khan <arbab.tufail@ocloudsolutions.net>
2023-02-02 11:33:10 +0500

    JS Group By Assignment
    This is JavaScript Task#1. In which converting an array of objects into a single object having a key based on Cities,

---
+++ GroupByAssignment.js
@@ -0,0 +1,27 @@
+//********* JavaScript-Task#1-GroupByAssignment *****************
+
+const array= [
+  {id:1,name:"Bilal", city:"Lahore"},
+  {id:1,name:"Bilal", city:"Lahore"},
+  {id:3,name:"Hafsa", city:"Karachi"},
+  {id:4,name:"Rehan", city:"Lahore"},
+  {id:5,name:"Saqib", city:"Karachi"},
+  {id:6,name:"Farhan", city:"Islamabad"}
+]
+
+
+  function func(array,key){
+      return array.reduce((emptyObject, element)=>{                
+          if((element[key] in emptyObject)===false) {  /*check if a key is already exist or does not exist in object_acumulator */
+              emptyObject[element[key]] = emptyObject[element[key]]   /* making key of object */ 
+              emptyObject[element[key]] = [];                         /* initilizing key_value to empty list*/ 
+          }   
+          emptyObject[element[key]].push(element);     /* adding array_objects into the list of keys */
+          return emptyObject;
+      },{})
+  }
+
+  console.log('\nGroupByCity:\n\n', func(array,'city'),'\n\n');
+
+
+  //********************************************************************************** */
