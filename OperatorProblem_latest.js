efa933aed83be69abd7b9e471bbd217760764474
Arbab Khan <arbab.tufail@ocloudsolutions.net>
2023-02-14 17:24:30 +0500

    Update OperatorProblem_latest.js
    Removed Extra line of Code

--- OperatorProblem_latest.js
+++ OperatorProblem_latest.js
@@ -10,19 +10,6 @@ const recursiveFunction = function () {
       for (let i = 0; i < element.length; i++) {
         if (Array.isArray(element[i])) {
           newArray.push(func(element[i])); //Checking if nested element is array or not//
-          if (newArray.length === 3) {
-            const oeprand2 = newArray.pop();
-            const operand1 = newArray.pop();
-            const operator = newArray.pop();
-            const makingInfixExpression = "".concat(
-              operand1,
-              operator,
-              oeprand2
-            );
-            newArray.push(makingInfixExpression);
-            const expression = newArray.pop();
-            return expression;
-          }
         } else {
           newArray.push(element[i]); /// if next element is not array then simply push it into the newArray
         }
