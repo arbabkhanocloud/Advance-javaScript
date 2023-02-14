0ef592c88b9ad4756fda834197a283cde465552c
Arbab Khan <arbab.tufail@ocloudsolutions.net>
2023-02-14 15:35:14 +0500

    Update GroupByAssignment_forLoop.js
    removed extra step of making object key. And directly assigning empty array to object key's value.

--- GroupByAssignment_forLoop.js
+++ GroupByAssignment_forLoop.js
@@ -1,22 +1,21 @@
-const array= [
-    {id:1,name:"Bilal", city:"Lahore"},
-    {id:1,name:"Bilal", city:"Lahore"},
-    {id:3,name:"Hafsa", city:"Karachi"},
-    {id:4,name:"Rehan", city:"Lahore"},
-    {id:5,name:"Saqib", city:"Karachi"},
-    {id:6,name:"Farhan", city:"Islamabad"}
-  ]
-
-    function func(array,key){
-        const emptyObject ={};
-        for( let element =0;  element < array.length; element++){
-            if((array[element][key] in emptyObject)===false) {  /*check if a key is already exist or does not exist in object_acumulator */
-                emptyObject[array[element][key]] = emptyObject[array[element][key]]   /* making key of object */ 
-                emptyObject[array[element][key]] = [];                         /* initilizing key_value to empty list*/ 
-                }   
-                emptyObject[array[element][key]].push(array[element]); 
-        }
-        return emptyObject;
+const array = [
+  { id: 1, name: "Bilal", city: "Lahore" },
+  { id: 1, name: "Bilal", city: "Lahore" },
+  { id: 3, name: "Hafsa", city: "Karachi" },
+  { id: 4, name: "Rehan", city: "Lahore" },
+  { id: 5, name: "Saqib", city: "Karachi" },
+  { id: 6, name: "Farhan", city: "Islamabad" },
+];
+function func(array, key) {
+  const emptyObject = {};
+  for (let elementIndex = 0; elementIndex < array.length; elementIndex++) {
+    const elementKey = array[elementIndex][key];
+    if (!(elementKey in emptyObject)) {
+      /*check if a key is already exist or does not exist in object_acumulator */
+      emptyObject[elementKey] = []; /*if not exist then making and initilizing key_value to empty list*/
     }
-    console.log('\nGroupByCity:\n\n', func(array,'city'),'\n\n');
-  
+    emptyObject[elementKey].push(array[elementIndex]);
+  }
+  return emptyObject;
+}
+console.log("\nGroupByCity:\n\n", func(array, "city"), "\n\n");
